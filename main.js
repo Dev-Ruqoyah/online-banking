a0ac4820609b6b37597b7bc45dda354b8980bd8b
bossLady0001 <hamzatrukayatabiodun02@gmail.com>
2024-06-13 18:14:12 -0700

    third commit

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('navbar');
+    var navLink = document.querySelectorAll('.nav-link')
+    var signUpBtn = document.querySelector('.signupbtn')
+    console.log(navbar);
     if (window.scrollY > 0) {
         navbar.classList.add('scrolled');
-    } else {
+        signUpBtn.classList.add('scrolled');
+
+        navLink.forEach(function(link) {
+            link.classList.add('scrolled');
+        });
+    }
+    else {
         navbar.classList.remove('scrolled');
+        signUpBtn.classList.remove('scrolled');
+        navLink.forEach(function(link) {
+            link.classList.remove('scrolled');
+        });
     }
 });
+
